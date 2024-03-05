97f00f3d4fdb435b94c44ad0db522b4e891f2921
Anastasiia Siutkina <120054681+anastasimars@users.noreply.github.com>
2024-03-05 20:45:21 +0100

    ASIT-001-Stworzenie templatki projektu springbootowego (#6)
    * add maven project
    
    * add .mvn to .gitignore

---
+++ src/main/java/pl/akademiaspecjalistowit/DocumentFlowManagementService/DocumentFlowManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.DocumentFlowManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentFlowManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentFlowManagementServiceApplication.class, args);
+	}
+
+}
