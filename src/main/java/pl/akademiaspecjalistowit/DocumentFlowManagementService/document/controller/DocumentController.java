14a1b6c7922dd4f32b59e4c919dafeb70127f7a8
anastasimars <sutckina.n@gmail.com>
2024-03-07 10:20:01 +0100

    added repo

--- src/main/java/pl/akademiaspecjalistowit/DocumentFlowManagementService/document/controller/DocumentController.java
+++ src/main/java/pl/akademiaspecjalistowit/DocumentFlowManagementService/document/controller/DocumentController.java
@@ -5,19 +5,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.akademiaspecjalistowit.DocumentFlowManagementService.document.service.DocumentService;
 import pl.akademiaspecjalistowit.DocumentFlowManagementService.document.dto.DocumentDto;
+import pl.akademiaspecjalistowit.DocumentFlowManagementService.document.service.DocumentService;
 
 @RestController
-@RequestMapping
+@RequestMapping("/documents")
 @AllArgsConstructor
 public class DocumentController {
 
-    private final DocumentService documentApi;
+    private final DocumentService documentService;
 
     @PostMapping
     void saveDocument(@RequestBody DocumentDto documentDto){
-        documentApi.saveDocument(documentDto);
+        documentService.saveDocument(documentDto);
     }
 
 }
